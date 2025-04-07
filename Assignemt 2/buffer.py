cf3e83858ebbd9ee6be022e74787ab455c2b15fa
Curtis Amo Dwira <amodwira@gmail.com>
2025-04-07 17:06:38 -0500

    Updated

--- Assignemt 2/buffer.py
+++ Assignemt 2/buffer.py
@@ -1,7 +1,7 @@
 import arcpy
 arcpy.analysis.Buffer(
-    in_features="no_retail.shp",
-    out_feature_class="./retail_buffer_1.shp",
+    in_features="Assignemt 2/no_retail.shp",
+    out_feature_class="D:\Curtis_assignment\geog4057\programming\geog4057_Curtis\Assignemt 2/retail_buffer_1.shp",
     buffer_distance_or_field="500 Meters",
     line_side="FULL",
     line_end_type="ROUND",
